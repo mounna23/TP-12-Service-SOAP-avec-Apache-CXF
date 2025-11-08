256129364bef801eafa6ac9e11c2764fba6e3258
mounna23 <mouna.eddaoudi@gmail.com>
2025-11-08 22:21:09 +0100

    first commit

---
+++ soap-cxf-service/src/main/java/com/acme/cxf/Server.java
@@ -0,0 +1,15 @@
+package com.acme.cxf;
+
+import com.acme.cxf.impl.HelloServiceImpl;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+
+public class Server {
+    public static void main(String[] args) {
+        String address = "http://localhost:8080/services/hello";
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+        factory.setServiceClass(HelloServiceImpl.class);
+        factory.setAddress(address);
+        factory.create();
+        System.out.println("WSDL: " + address + "?wsdl");
+    }
+}
